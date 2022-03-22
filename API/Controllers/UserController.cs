dd7aa7aa445b0e8330b9503716c9197bd1287259
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-03-22 08:03:14 +0200

    Implemented controller, model and DB context

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public UserController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        
+        
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return Ok(await _dataContext.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(Guid id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return BadRequest("user not found");
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> AddUser(User user) // if the object reciev as parameter is a primitive data type [FromBody] is required 
+        {
+
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+            
+
+            return Ok(_dataContext.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User request)
+        {
+            var user = await _dataContext.Users.FindAsync(request.Id);
+            if (user == null)
+            {
+                return BadRequest("user not found");
+            }
+
+            user.Username = request.Username;
+            user.Password = request.Password;
+            user.Email = request.Email;
+            
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(Guid id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+            if (user == null)
+            {
+                return BadRequest("user not found");
+            }
+            _dataContext.Users.Remove(user);
+            await _dataContext.SaveChangesAsync();
+            return Ok(_dataContext.Users.ToListAsync());
+
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opsions) : base(opsions)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
