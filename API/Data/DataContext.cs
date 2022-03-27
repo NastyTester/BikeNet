f1c395a6989f1885869be17ab0f4d429a5b6ecef
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-03-27 18:06:31 +0300

    Added post controller

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,14 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> opsions) : base(opsions)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
     }
 }
---
+++ API/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public Guid? UserId { get; set; }
+    }
+}
