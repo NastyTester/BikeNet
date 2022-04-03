9297cc21f48f35b8384966cd9ee702f2ca37f7a6
NastyTester <70013669+NastyTester@users.noreply.github.com>
2022-04-03 15:11:57 +0300

    deleted default api end points

--- application/src/Components/Feed.js
+++ application/src/Components/Feed.js
@@ -1,12 +1,31 @@
 import NavBar from "./NavBar";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 export default function Feed() {
+    const [data, setData] = useState({ posts: [] });
 
+    useEffect(async () => {
+        const result = await axios(
+          'https://localhost:7134/Post/feed',
+        );
+            console.log(result)
+        setData(result.data);
+      });
+
+    console.log(data);
 
     return(
         <div>
         <NavBar/>
+            <ul>
+                {data.posts.map(item => (
+                <li key={item.Id}>
+                    <p>item.Title</p>
+                </li>
+                ))}
+            </ul>
         Soon data will be shown here
         </div>
     )
